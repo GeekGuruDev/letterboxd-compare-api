0d06fc907fd3738e009497acbd175436aa1a5eae
Your Name <progurpreetsingh@gmail.com>
2025-03-29 13:30:04 +0530

    Cofig cors and remove status interval log

--- app.js
+++ app.js
@@ -12,7 +12,12 @@ const {
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://letterboxdcompare.netlify.app/",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  })
+);
 app.get("/profile/:username", getProfile);
 app.get("/movies/:username", getMovies);
 app.get("/watchlist/:username", getWatchlist);
@@ -20,11 +25,11 @@ app.get("/reviews/:username", getReviews);
 app.get("/movie/:slug", getMovie);
 app.get("/status", getStatus);
 
-setInterval(async () => {
-  const res = await fetch("https://letterboxd-compare-api.onrender.com/status");
-  const data = await res.json();
-  console.log(data.status, ":", data.message);
-}, 10 * 60 * 1000);
+// setInterval(async () => {
+//   const res = await fetch("https://letterboxd-compare-api.onrender.com/status");
+//   const data = await res.json();
+//   console.log(data.status, ":", data.message);
+// }, 10 * 60 * 1000);
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
