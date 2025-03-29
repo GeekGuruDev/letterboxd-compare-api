7a91a1a137db907a08b15d75c2bb8953bea814b8
Your Name <progurpreetsingh@gmail.com>
2025-03-29 13:33:31 +0530

    Fix cors issue

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "https://letterboxdcompare.netlify.app/",
+    origin: "https://letterboxdcompare.netlify.app",
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 );
